070f9505be92a82adea5e783c4521945be17aed6
Rebecca Murray <rmm2242@columbia.edu>
2020-07-04 18:11:05 -0400

    adding animations

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// iconStyle = {
-//   &:hover: {
-//       transform: translateX(5px);
-//   }
-// }
+const imgStyle = {
+  transition: "transform 330ms ease-out",
+  cursor: "pointer",
+  "&:hover": {
+    transform: "translateY(-10px)",
+  }
+}
+
 
 function App() {
+
+  var [isGridLayout, setisGridLayout] = useState(true);
+  const toggleIsGridLayout = () => setisGridLayout(!isGridLayout);
+
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20vh" }}>
       <div><h1>Rebecca Murray</h1></div>
-      <div style={{ display: "flex", flexDirection: "column" }}>
         <div>
-          <img src="https://img.icons8.com/clouds/100/000000/graduation-cap.png" />
-          <img src="https://img.icons8.com/clouds/100/000000/business.png" />
-          <img src="https://img.icons8.com/clouds/100/000000/resume.png" />
+          <img 
+            onClick={toggleIsGridLayout} 
+            src="https://img.icons8.com/clouds/100/000000/graduation-cap.png" 
+            style={isGridLayout ? {transform: "translateX(150%)"} : {}}
+          />
+          <a href="https://github.com/RebeccaMurray" target="_blank">
+          <img 
+            src="https://img.icons8.com/clouds/100/000000/github.png" 
+            style={isGridLayout ? {transform: "translateY(100%) translateX(50%)"} : {}}
+          />
+          </a>
+          <img 
+            onClick={toggleIsGridLayout} 
+            src="https://img.icons8.com/clouds/100/000000/business.png"
+            style={isGridLayout ? {transform: "translateX(50%)"} : {}}
+           />
+          <a href="https://www.linkedin.com/in/rebeccamichellemurray/" target="_blank">
+          <img 
+            style={isGridLayout ? {transform: "translateY(100%) translateX(-50%)"} : {}}
+            src="https://img.icons8.com/clouds/100/000000/linkedin.png" 
+          />
+          </a>
+          <img 
+            onClick={toggleIsGridLayout} 
+            src="https://img.icons8.com/clouds/100/000000/resume.png" 
+            style={isGridLayout ? {transform: "translateX(-50%)"} : {}}
+          />
+          
+          <img 
+            onClick={toggleIsGridLayout} 
+            src="https://img.icons8.com/clouds/100/000000/new-post.png"
+            style={isGridLayout ? {transform: "translateY(100%) translateX(-150%)"} : {}}
+          />
         </div>
-        <div>
-          <img src="https://img.icons8.com/clouds/100/000000/github.png" />
-          <img src="https://img.icons8.com/clouds/100/000000/linkedin.png" />
-          <img src="https://img.icons8.com/clouds/100/000000/new-post.png" />
-        </div>
-      </div>
     </div>
   );
 }
