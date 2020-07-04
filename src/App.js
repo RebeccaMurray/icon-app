a59c0889dd9355d6aa6d79413340c56fc535eadf
Rebecca Murray <rmm2242@columbia.edu>
2020-07-04 14:18:12 -0400

    expandable left panel

--- src/App.js
+++ src/App.js
@@ -1,68 +1,38 @@
 import React from 'react';
 import { useEffect, useState, useRef } from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import { Flipper, Flipped } from 'react-flip-toolkit'
+import { wrapGrid } from 'animate-css-grid'
 
-// import { spring } from 'react-flip-toolkit'
-
-// spring({
-//   config: "wobbly",
-//   values: {
-//     translateY: [-15, 0],
-//     opacity: [0, 1]
-//   },
-//   // delay: i * 25,
-//   onComplete: () => console.log('done')
-// });
-
-const colStyle = { flexDirection: "row" };
-const rowStyle = { flexDirection: "column" };
 
 function App() {
 
-  // useEffect(() => {
-  //   const grid = document.getElementById("grid");
-  //   wrapGrid(grid);
-  // }, []);
   const [viewingSection, setViewingSection] = useState(false);
   const gridRef = useRef(null);
+
+  useEffect(() => {
+    wrapGrid(gridRef.current);
+  }, []);
+  
   const toggleViewingSection = () => {
     setViewingSection(prevState => !prevState);
-    gridRef.current.classList.toggle('is-viewing-section')
+    gridRef.current.classList.toggle('is-viewing-section');
     
   }
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20vh" }}>
       <div><h1>Rebecca Murray</h1></div>
-      {/* <Flipper flipKey={String(viewingSection)} spring="gentle" staggerConfig={{
-        default: {
-          reverse: true,
-          speed: 0.2,
-        }
-      }}> */}
-        <div className="grid" ref={gridRef} style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-          {/* <Flipped flipId="grad"> */}
+        <div className="grid" ref={gridRef} >
+            <div id="blank-left" style={{backgroundColor: "blue"}}></div>
             <img id="grad" onClick={toggleViewingSection} src="https://img.icons8.com/clouds/100/000000/graduation-cap.png" />
-          {/* </Flipped> */}
-          {/* <Flipped flipId="business"> */}
+
             <img id="business" onClick={toggleViewingSection} src="https://img.icons8.com/clouds/100/000000/business.png" />
-          {/* </Flipped> */}
-          {/* <Flipped flipId="resume"> */}
             <img id="resume" onClick={toggleViewingSection} src="https://img.icons8.com/clouds/100/000000/resume.png" />
-          {/* </Flipped> */}
-          {/* <Flipped flipId="github"> */}
             <img id="github" onClick={toggleViewingSection} src="https://img.icons8.com/clouds/100/000000/github.png" />
-          {/* </Flipped> */}
-          {/* <Flipped flipId="linkedin"> */}
             <img id="linkedin" onClick={toggleViewingSection} src="https://img.icons8.com/clouds/100/000000/linkedin.png" />
-          {/* </Flipped> */}
-          {/* <Flipped flipId="email"> */}
             <img id="email" onClick={toggleViewingSection} src="https://img.icons8.com/clouds/100/000000/new-post.png" />
-          {/* </Flipped> */}
+            {/* <div id="blank-right" style={{backgroundColor: "green"}}></div> */}
         </div>
-      {/* // </Flipper> */}
     </div>
   );
 }
